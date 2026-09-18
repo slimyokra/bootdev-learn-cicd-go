ce365b059e59778e1d76cbfccb2ea75542ef5164
slimyokra <slimyokra123@gmail.com>
2026-09-18 21:25:07 +0000

    ch5 l3

--- json.go
+++ json.go
@@ -27,8 +27,11 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	if err != nil {
 		log.Printf("Error marshalling JSON: %s", err)
 		w.WriteHeader(500)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	if _, err := w.Write(dat); err != nil {
+		log.Printf("Error writing response: %s", err)
+	}
 }
--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -30,10 +32,15 @@ func main() {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
 	}
 
-	port := os.Getenv("PORT")
-	if port == "" {
+	portEnv := os.Getenv("PORT")
+	if portEnv == "" {
 		log.Fatal("PORT environment variable is not set")
 	}
+	portNum, err := strconv.Atoi(portEnv)
+	if err != nil {
+		log.Fatal("PORT environment variable must be numeric")
+	}
+	port := strconv.Itoa(portNum)
 
 	apiCfg := apiConfig{}
 
@@ -89,8 +96,9 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: 5 * time.Second,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
